adaf4383abfcee14e969e74bd454bca526488d51
eduardo-moro <dev.eduardomoro@gmail.com>
2025-09-24 18:09:10 -0300

    revert

--- solution/services/api-write/cmd/main.go
+++ solution/services/api-write/cmd/main.go
@@ -1,52 +1,26 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
-	"time"
+	"os"
 
-	"avenue-golang/services/api-write/internal/config"
 	"avenue-golang/services/api-write/internal/handlers"
 	"avenue-golang/services/api-write/internal/services"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	cfg := config.LoadConfig()
-
-	// Connect to MongoDB
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MongoURI))
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
-		}
-	}()
-
-	// Connect to RabbitMQ
-	ramqConn, err := amqp.Dial(cfg.RabbitMQURI)
-	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
-	}
-	defer ramqConn.Close()
-
-	log.Println("Connected to MongoDB and RabbitMQ")
-
-	svc := services.NewService(client, ramqConn)
+	svc := services.NewService()
 	h := handlers.NewHandler(svc)
 
 	http.HandleFunc("/health", h.HealthCheck)
 	http.HandleFunc("/events", h.EventsHandler)
 
-	log.Printf("API Write Service listening on port %s", cfg.Port)
-	log.Fatal(http.ListenAndServe(":"+cfg.Port, nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("API Write Service listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- solution/services/api-write/internal/services/services.go
+++ solution/services/api-write/internal/services/services.go
@@ -1,19 +1,8 @@
 package services
 
-import (
-	"go.mongodb.org/mongo-driver/mongo"
-	amqp "github.com/rabbitmq/amqp091-go"
-)
-
 type Service struct {
-	// Add MongoDB and RabbitMQ connections here
-	mongoClient *mongo.Client
-	ramqConn    *amqp.Connection
 }
 
-func NewService(mongoClient *mongo.Client, ramqConn *amqp.Connection) *Service {
-	return &Service{
-		mongoClient: mongoClient,
-		ramqConn:    ramqConn,
-	}
+func NewService() *Service {
+	return &Service{}
 }
